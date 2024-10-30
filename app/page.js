7d28810a607b14f67c068bd85051b867299eeede
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-10-30 12:42:20 +0400

    blur on mouse

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
-import Button from "./components/buttonSecondary/button/Button";
+"use client";
+import { useEffect, useState } from "react";
+import Button from "./components/button/Button";
+import "./styles/globals.css";
+
 export default function Home() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
-    <div>
+    <div className="Portfolio">
       <h1>zd all</h1>
       <h2>zd all</h2>
       <h3>zd all</h3>
@@ -9,6 +25,10 @@ export default function Home() {
       <Button>ragac</Button>
       <Button>submit</Button>
       <Button>submit your text please</Button>
+      <div
+        className="blur-effect"
+        style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      ></div>
     </div>
   );
 }
