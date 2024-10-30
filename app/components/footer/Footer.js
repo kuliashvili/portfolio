8a7cb9438d536891d755a519f2d8f6dd12ed8623
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-10-30 17:56:09 +0400

    footer component

---
+++ app/components/footer/Footer.js
@@ -0,0 +1,51 @@
+import "./Footer.css";
+import githubIcon from "../../assets/github.svg";
+import linkedinIcon from "../../assets/linkedin.svg";
+import Image from "next/image";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-socials">
+          <a
+            className="footer-socials-item"
+            href="https://github.com/kuliashvili"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={githubIcon}
+              width={30}
+              height={30}
+              alt="Github Icon"
+            ></Image>
+          </a>
+          <a
+            className="footer-socials-item"
+            href="https://www.linkedin.com/in/kuliashvili/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={linkedinIcon}
+              width={30}
+              height={30}
+              alt="Linkedin Icon"
+            ></Image>
+          </a>
+        </div>
+
+        <div className="footer-email">
+          <p>
+            <a href="mailto:giorgi.kuliashvili.1@btu.edu.ge">
+              giorgi.kuliashvili.1@btu.edu.ge
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Button from "./components/button/Button";
 import "./styles/globals.css";
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer"
 
 export default function Home() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -20,13 +21,8 @@ export default function Home() {
   return (
     <div className="Portfolio">
       <Header></Header>
-      <h1>zd all</h1>
-      <h2>zd all</h2>
-      <h3>zd all</h3>
-      <p>zd all</p>
-      <Button>ragac</Button>
-      <Button>submit</Button>
-      <Button>submit your text please</Button>
+      <Button></Button>
+      <Footer></Footer>
       <div
         className="blur-effect"
         style={{ left: `${position.x}px`, top: `${position.y}px` }}
