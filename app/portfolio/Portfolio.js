91482e1995fbf156d7b65de3b7361817afb6c18b
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-11-09 23:50:21 +0400

    make portfolio code prettyer

--- app/portfolio/Portfolio.js
+++ app/portfolio/Portfolio.js
@@ -1,12 +1,11 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Portfolio() {
-
   return (
     <div>
-     <h1>Portfolio page</h1>
+      <h1>Portfolio page</h1>
     </div>
   );
 }
--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,7 +1,8 @@
 import Portfolio from "./Portfolio";
+
 export const metadata = {
-  title: 'My Portfolio',
-  description: 'Welcome to my portfolio page showcasing my projects and skills'
+  title: "My Portfolio",
+  description: "Welcome to my portfolio page showcasing my projects and skills",
 };
 
 export default function PortfolioPage() {
