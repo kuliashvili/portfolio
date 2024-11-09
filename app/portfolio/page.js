18d75a2e1a394d2dd0ad4faf5d1401dc41608264
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-11-09 23:49:52 +0400

    metadata on portfolio page, example for other pages

---
+++ app/portfolio/page.js
@@ -0,0 +1,13 @@
+import Portfolio from "./Portfolio";
+export const metadata = {
+  title: 'My Portfolio',
+  description: 'Welcome to my portfolio page showcasing my projects and skills'
+};
+
+export default function PortfolioPage() {
+  return (
+    <main>
+      <Portfolio />
+    </main>
+  );
+}
