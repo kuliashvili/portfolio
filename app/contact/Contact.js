482fa41a29cd9061e543b00568c9c07914537f5a
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-11-10 09:51:53 +0400

    contact page metadata

---
+++ app/contact/Contact.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact page</h1>
+    </div>
+  );
+}
+
---
+++ app/contact/page.js
@@ -0,0 +1,15 @@
+import Contact from "./Contact";
+
+export const metadata = {
+  title: "Contact me",
+  description: "Welcome to my contact page",
+};
+
+export default function PortfolioPage() {
+  return (
+    <main>
+      <Contact />
+    </main>
+  );
+}
+
