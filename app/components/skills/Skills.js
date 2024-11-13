a4df9923d3efd5493ecc12f12c4f2007283d441c
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-11-13 14:49:12 +0400

    modify skills component

--- app/components/skills/Skills.js
+++ app/components/skills/Skills.js
@@ -1,10 +1,5 @@
 import "./Skills.css";
 import Image from "next/image";
-import Html from "../../../public/assets/html.svg";
-import Css from "../../../public/assets/css.svg";
-import JavaScript from "../../../public/assets/js.svg";
-import React from "../../../public/assets/react.svg";
-import NextJs from "../../../public/assets/next.svg";
 import Html2 from "../../../public/assets/html2.svg";
 import Css2 from "../../../public/assets/css2.svg";
 import JavaScript2 from "../../../public/assets/js2.svg";
@@ -12,43 +7,52 @@ import React2 from "../../../public/assets/react2.svg";
 import NextJs2 from "../../../public/assets/next2.svg";
 
 function Skills() {
+  const skillImages = [
+    { src: Html2, alt: "Html Icon" },
+    { src: Css2, alt: "Css Icon" },
+    { src: JavaScript2, alt: "JavaScript Icon" },
+    { src: React2, alt: "React Icon" },
+    { src: NextJs2, alt: "NextJs Icon" },
+    { src: Html2, alt: "Html Icon" },
+    { src: Css2, alt: "Css Icon" },
+    { src: JavaScript2, alt: "JavaScript Icon" },
+    { src: React2, alt: "React Icon" },
+    { src: NextJs2, alt: "NextJs Icon" },
+    { src: Html2, alt: "Html Icon" },
+    { src: Css2, alt: "Css Icon" },
+    { src: JavaScript2, alt: "JavaScript Icon" },
+    { src: React2, alt: "React Icon" },
+    { src: NextJs2, alt: "NextJs Icon" },
+    { src: Html2, alt: "Html Icon" },
+    { src: Css2, alt: "Css Icon" },
+    { src: JavaScript2, alt: "JavaScript Icon" },
+    { src: React2, alt: "React Icon" },
+    { src: NextJs2, alt: "NextJs Icon" },
+    { src: Html2, alt: "Html Icon" },
+    { src: Css2, alt: "Css Icon" },
+    { src: JavaScript2, alt: "JavaScript Icon" },
+    { src: React2, alt: "React Icon" },
+    { src: NextJs2, alt: "NextJs Icon" },
+  ];
+
+  const SkillSet = ({ className }) => (
+    <div className={className}>
+      {skillImages.map((img, index) => (
+        <Image
+          key={index}
+          className="skills-image"
+          src={img.src}
+          width={35}
+          height={86}
+          alt={img.alt}
+        />
+      ))}
+    </div>
+  );
+
   return (
-    <div className="skills">
-      <Image
-        className="skills-image"
-        src={Html2}
-        width={35}
-        height={86}
-        alt="Html Icon"
-      ></Image>
-      <Image
-        className="skills-image"
-        src={Css2}
-        width={35}
-        height={86}
-        alt="Css Icon"
-      ></Image>
-      <Image
-        className="skills-image"
-        src={JavaScript2}
-        width={35}
-        height={86}
-        alt="JavaScript Icon"
-      ></Image>
-      <Image
-        className="skills-image"
-        src={React2}
-        width={35}
-        height={86}
-        alt="React Icon"
-      ></Image>
-      <Image
-        className="skills-image"
-        src={NextJs2}
-        width={35}
-        height={86}
-        alt="NextJs Icon"
-      ></Image>
+    <div className="skills-container">
+      <SkillSet className="skills" />
     </div>
   );
 }
