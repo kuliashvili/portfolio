5a281921bda7027c44c0995e06707a838ba3a50f
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2024-11-26 00:05:22 +0400

    blog page UI

--- app/blog/Blog.js
+++ app/blog/Blog.js
@@ -1,6 +1,7 @@
 "use client";
 import "./blog.css";
-
+import Coding from "@/public/assets/coding.svg";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function Blog() {
@@ -8,10 +9,20 @@ export default function Blog() {
     <div className="blogs">
       <h1 className="blog-header">Coming Soon!</h1>
       <p>
-        Thanks for visiting! I’m currently working on some exciting blog posts
-        about design, web development, and creativity. Check back soon for fresh
-        content!
+        Thanks for visiting!
+        <br /> I'm currently working on some exciting blog posts<br />
+        about design, web development, and creativity. Check back soon for
+        <br /> fresh content!
       </p>
+      <div className="image-container">
+        <Image
+          className="coding-image"
+          src={Coding}
+          width={250}
+          height={250}
+          alt="Coding Image"
+        />
+      </div>
     </div>
   );
 }
